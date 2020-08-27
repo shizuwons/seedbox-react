877627a304b17fa5bc8922bfc63418dcf327794b
Vince Christian Dilig <dilig.vinz16@gmail.com>
2020-08-27 11:57:22 +0800

    favicon

--- components/meta.js
+++ components/meta.js
@@ -14,6 +14,7 @@ export default ({
             />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta charSet="utf-8" />
+            <link rel="shortcut icon" href="Image\logo_only.jpg" type="image/x-icon"/>
 
             <link 
                 href="Css/bootstrap.min.css"
